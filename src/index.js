2ee24d04208d2a5d8ec9795f28a0175e571700cc
Rahul Sharma <90137rahul@gmail.com>
2021-01-18 02:29:55 +0530

    ✨ Introduced Logut router

--- src/index.js
+++ src/index.js
@@ -7,20 +7,6 @@ const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-//   if (req.method === "GET") {
-//     res.send("GET REQUESTS ARE DISABLED");
-//   } else {
-//     next();
-//   }
-// });
-
-// app.use((req, res, next) => {
-//   if (req.method) {
-//     res.status(503).send("SITE UNDER MAINTENANCE");
-//   }
-// });
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -28,15 +14,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
-
-const myFunction = async () => {
-  const token = jwt.sign({ _id: "abc123" }, "thisismynewcourse", {
-    expiresIn: "7 days",
-  });
-  console.log(token);
-
-  const data = jwt.verify(token, "thisismynewcourse");
-  console.log(data);
-};
-
-myFunction();
